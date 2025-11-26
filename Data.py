f00161720ca06d0c231ebe0d38f6c52baa356999
Arkunir <abricotpomme1234@gmail.com>
2025-11-26 09:00:37 +0100

    Update Data.py

--- Data.py
+++ Data.py
@@ -8,6 +8,16 @@ class Start(Node):
         self.inputs = 0
         self.outputs = 1
 
+class End(Node):
+    def __init__(self):
+        self.inputs = 1
+        self.outputs = 0
+
+class Not(Node):
+    def __init__(self):
+        self.inputs = 1
+        self.outputs = 1
+
 class And(Node):
     def __init__(self):
         self.inputs = 2
@@ -33,11 +43,6 @@ class Nxor(Node):
         self.inputs = 2
         self.outputs = 1
 
-class End(Node):
-    def __init__(self):
-        self.inputs = 1
-        self.outputs = 0
-
 class ImpA(Node):
     def __init__(self):
         self.inputs = 2
@@ -47,3 +52,44 @@ class ImpB(Node):
     def __init__(self):
         self.inputs = 2
         self.outputs = 1
+
+def comp_and(input_a, input_b):
+    if input_a == input_b == True:
+        return True
+    else:
+        return False
+
+def comp_not(input_a):
+    if input_a == False:
+        return True
+    else:
+        return False
+
+def comp_or(input_a, input_b):
+    return comp_not(comp_and(comp_not(input_a), comp_not(input_b)))
+
+def comp_nand(input_a, input_b):
+    return comp_not(comp_and(input_a, input_b))
+
+def comp_xor(input_a, input_b):
+    return comp_and(comp_nand(input_a, input_b), comp_or(input_a, input_b))
+
+def comp_nor(input_a, input_b):
+    return comp_not(comp_or(input_a, input_b))
+
+def comp_nxor(input_a, input_b):
+    return comp_not(comp_xor(input_a, input_b))
+
+def comp_impa(input_a, input_b):
+    return comp_nor(comp_not(input_a), input_b)
+
+def comp_impb(input_a, input_b):
+    return comp_nor(input_a, comp_not(input_b))
+
+def truth_table():
+    return comp_and(a,a), comp_and(a,b), comp_and()
+
+a = True
+b = False
+
+print(comp_impa(a, a))
