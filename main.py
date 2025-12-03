f94c79ccd60b2a1e51d02400e98b06396b1b5399
soubrie <soubrie@PDG.LOCAL>
2025-12-03 08:13:49 +0100

    font

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from modules.ui import Window, EditorView, GameView
 import platform
 import os
 from modules.data import data
+import arcade
+
+arcade.load_font("assets/press_start.ttf")
 
 windows = Window()
 data.window = windows
